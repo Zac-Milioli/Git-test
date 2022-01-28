a05cae03b58bccdc82f66d094ed0224bf4b4c575
ZacMilioli <zacmilioli@gmail.com>
2022-01-28 14:16:33 -0300

    Função CPF dos clientes

--- funct.py
+++ funct.py
@@ -1,23 +1,27 @@
 class Data_log:
-    def __init__(self, log_num, list_name, list_password):
+    def __init__(self, log_num, list_name, list_password, list_cpf):
         self.log_num = log_num
         self.list_name = list_name
         self.list_password = list_password
+        self.list_cpf = list_cpf
 
     def register(self):
         for i in range(0, self.log_num):
             print(f'\n{i + 1}° :')
             name = input('\nNome de usuário: ')
             password = input('\nSenha: ')
+            cpf = str(input('\nCPF: '))
             self.list_name.append(name)
             self.list_password.append(password)
+            self.list_cpf.append(cpf)
 
     def users(self):
         print('\nUSUÁRIOS REGISTRADOS')
         print('=' * 8)
         for i in range(0, self.log_num):
             print(f'\nUsername - {self.list_name[i]}')
-            print(f'Password - {self.list_password[i]}\n')
+            print(f'Password - {self.list_password[i]}')
+            print(f'CPF - {self.list_cpf[i]}\n')
 
     def add_user(self):
         log_num_sum = int(input('Quantos novos serão registrados? '))
@@ -25,17 +29,28 @@ class Data_log:
             print(f'\n{self.log_num + i + 1}° :')
             name = input('\nNome de usuário: ')
             password = input('\nSenha: ')
+            cpf = input('\nCPF: ')
             print('\n')
             self.list_name.append(name)
             self.list_password.append(password)
+            self.list_cpf.append(cpf)
         self.log_num += log_num_sum
 
     def del_user(self):
         num = int(input('Quantos usuários serão deletados? '))
         for i in range(0, num):
-            name_del = str(input('\nNome do usuário a ser deletado: '))
-            log_position = self.list_name.index(name_del)
-            self.list_name.pop(log_position)
-            self.list_password.pop(log_position)
-            self.log_num -= 1
-
+            opt = int(input('[1] Excluir por CPF / [2] Excluir por nome\n'))
+            if opt == 1:
+                cpf_del = str(input('\nCPF do usuário a ser deletado: '))
+                log_position = self.list_cpf.index(cpf_del)
+                self.list_name.pop(log_position)
+                self.list_password.pop(log_position)
+                self.list_cpf.pop(log_position)
+                self.log_num -= 1
+            elif opt == 2:
+                name_del = input('\nNome do usuário a ser deletado: ')
+                log_position = self.list_name.index(name_del)
+                self.list_name.pop(log_position)
+                self.list_password.pop(log_position)
+                self.list_cpf.pop(log_position)
+                self.log_num -= 1
--- projetinho_python.py
+++ projetinho_python.py
@@ -7,7 +7,8 @@ print('- '*20)
 log_num = int(input('Quantos usuários serão registrados? '))
 list_name = []
 list_password = []
-obj_user = Data_log(log_num, list_name, list_password)
+list_cpf = []
+obj_user = Data_log(log_num, list_name, list_password, list_cpf)
 
 obj_user.register()
 
@@ -30,9 +31,7 @@ while True:
         obj_user.users()
     elif opt == 2:
         obj_user.add_user()
-        obj_user.users()
     elif opt == 3:
         obj_user.del_user()
-        obj_user.users()
     elif opt == 4:
         break
